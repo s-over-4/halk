a0f697a3059cc5173d60beaf9d3a513a0edfbda1
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-05-04 15:10:59 -0400

    HALK HALJ HALK

--- src/tree.c
+++ src/tree.c
@@ -1,7 +1,15 @@
 #include "include/tree.h"
 
 TREE_t* tree_init(int type) {
+   // give the tree just enough room
    TREE_t* tree = calloc(1, sizeof(struct TREE_STRUC));
+
+   tree->type = type;
+
+   tree->VAR_DEF_name      = NULL;
+   tree->VAR_DEF_value     = NULL;
+   tree->VAR_name          = NULL;
+
 
    return tree;
 }
