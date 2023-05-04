56d409f7df528511e96fada2bc8fea79f581c7df
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-05-04 13:09:48 -0400

    halk? :(

---
+++ src/include/tree.h
@@ -0,0 +1,25 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdlib.h>
+
+typedef struct TREE_STRUC {
+   enum {
+      TREE_VAR_DEF,
+      //TREE_FUN_DEF,
+      TREE_VAR,
+      //TREE_FUN_CALL,
+      //TREE_STR,
+      //TREE_COMP
+   } type;
+
+   char* VAR_DEF_name;
+   struct TREE_STRUC* VAR_DEF_value;
+
+   char* VAR_name;
+
+} TREE_t;
+
+TREE_t tree_init(int type);
+
+#endif
