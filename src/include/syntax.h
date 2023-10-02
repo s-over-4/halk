4d9dd289951589ddf408fdec62245b20cfe199c7
c+1 <s-over-4@githubusercontent.github.com>
2023-10-02 13:13:45 -0400

    syntax.h

---
+++ src/include/syntax.h
@@ -0,0 +1,24 @@
+#ifndef SYNTAX_H
+#define SYNTAX_H
+
+/* Syntax Definitions */
+
+#define SYNTAX_CHAR_DELIM        '\''
+#define SYNTAX_STR_DELIM         '"'
+#define SYNTAX_COMMENT_DELIM     '`'
+#define SYNTAX_EXPR_END          ';'
+#define SYNTAX_SET               '='
+#define SYNTAX_LGROUP            '('
+#define SYNTAX_RGROUP            ')'
+#define SYNTAX_DIRECTIVE_DELIM   '#'
+#define SYNTAX_APPLY             '.'
+#define SYNTAX_LIST_DELIM        ','
+#define SYNTAX_TAG_DELIM         ':'
+#define SYNTAX_NAMESPACE_DELIM   '/'
+#define SYNTAX_LBLOCK            '{'
+#define SYNTAX_RBLOCK            '}'
+#define SYNTAX_RLIST             '['
+#define SYNTAX_LLIST             ']'
+#define SYNTAX_ESC               '\\'
+
+#endif
